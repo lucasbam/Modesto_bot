e9a2aeeab5a96afd75b5ccf75d86bcca04c1932a
lucasbam <lbufpe@gmail.com>
2017-03-23 22:07:09 -0300

    Token mudado, status mudado.

--- src/main/java/BarrosCompany/ModestoDiscord/ModestoBot.java
+++ src/main/java/BarrosCompany/ModestoDiscord/ModestoBot.java
@@ -5,16 +5,18 @@ import BarrosCompany.ModestoDiscord.Handlers.EventHandler;
 import sx.blah.discord.api.ClientBuilder;
 
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.Status;
 import sx.blah.discord.util.DiscordException;
 
 public class ModestoBot {
-	static String Token = "MjgxNTcwNjc3Mjk4MDM2NzM2.C4Z6lg.5deyw9buVaqMxS-VIRnrFjpws8s";
+	static String Token = "Mjk0NjM3MzA0NDQwNjg0NTQ2.C7YCNw.eEMoyRHGWw5R9PIur-VhveeyqSk";
 	
 	public static IDiscordClient Bot;
 
 	public static void main(String[] args) throws Exception {
 		Bot = getClient(Token);
 		Bot.getDispatcher().registerListener(new EventHandler());
+		Bot.changeStatus(Status.game("%dc || %dc quit || feito por Lucas Barros"));
 		new dbManager();
 	}
 	
